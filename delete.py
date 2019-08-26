9c5b23b78cff3de8fe46d4202223b9d0d6fe226f
CJ Eller <45696734+cjeller1592@users.noreply.github.com>
2019-08-26 17:50:44 -0400

    Create delete.py

---
+++ delete.py
@@ -0,0 +1,41 @@
+import writeas
+
+# PUT IN VALUES BEFORE RUNNING SCRIPT
+# Username and password for Write.as account
+username = 'cjeller'
+password = 'Iwillnotmakeboring1'
+# The Write.as slug for your blog - ex: write.as/matt -> 'matt'
+collection = 'conversation'
+
+
+# Instanciate Write.as Client
+c = writeas.client()
+
+# Log in with Write.as
+user = c.login(username, password)
+c.setToken(user['access_token'])
+
+list = []
+
+# I assume 500 posts is more than generous!
+for i in range(1,50):
+# Iterate through the pages
+  cposts = c.retrieveCPosts('feed-test', i)
+  
+  posts = cposts['posts']
+    # If the posts are not an empty list, take each post and put it in a list!
+    # That way it catches pages that don't have 10 posts
+  if posts != []:
+     for post in posts:
+    # Append to the list of posts
+     list.append(post)
+
+  else:
+    break
+
+for p in list:
+  id = p['id']
+
+c.deletePost(id)
+
+print('All done! Posts deleted!')
